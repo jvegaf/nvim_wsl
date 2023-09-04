033907f3cc3a855abfc9e49f6d0a4f335fa4546c
Jose Vega <josevega234@gmail.com>
2023-09-04 19:27:50 +0200

    options

--- lua/config/base.lua
+++ lua/config/base.lua
@@ -1,35 +1,24 @@
-vim.opt.nu = true
-vim.opt.relativenumber = true
-
-vim.opt.tabstop = 2
-vim.opt.softtabstop = 2
-vim.opt.shiftwidth = 2
-vim.opt.expandtab = true
-
-vim.opt.smartindent = true
-
-vim.opt.wrap = false
-
-vim.opt.swapfile = false
 vim.opt.backup = false
-vim.opt.undofile = true
-
-vim.opt.incsearch = true
-vim.opt.showmode = true
-
-vim.opt.scrolloff = 8
-vim.opt.signcolumn = "yes"
-
-vim.opt.isfname:append("@-@")
-
-vim.opt.updatetime = 50
-
 vim.opt.clipboard = "unnamedplus"
-
-
+vim.opt.expandtab = true
 vim.opt.foldcolumn = '1'
+vim.opt.foldenable = true
 vim.opt.foldlevel = 99
 vim.opt.foldlevelstart = 99
-vim.opt.foldenable = true
+vim.opt.incsearch = true
+vim.opt.isfname:append("@-@")
+vim.opt.nu = true
+vim.opt.relativenumber = true
+vim.opt.scrolloff = 8
+vim.opt.shiftwidth = 2
+vim.opt.showmode = true
+vim.opt.signcolumn = "yes"
+vim.opt.smartindent = true
+vim.opt.softtabstop = 2
+vim.opt.swapfile = false
+vim.opt.tabstop = 2
+vim.opt.undofile = true
+vim.opt.updatetime = 50
+vim.opt.wrap = false
 
 vim.o.fillchars = [[eob: ,fold: ,foldopen:,foldsep: ,foldclose:]]
